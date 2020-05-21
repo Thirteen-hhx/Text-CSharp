ae4ceb2b076c6e35e3de834d5242df5fdd30f8af
Thirteen-hhx <57333574+Thirteen-hhx@users.noreply.github.com>
2020-05-21 18:29:39 +0800

    Add files via upload

---
+++ TimeDown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeDown : MonoBehaviour
+{
+    
+    public float CountDownTime;  //填写需要的时间，按秒计算，600秒就是10：00
+    private float GameTime;
+    private float timer = 0;
+    public Text GameCountTimeText;
+
+    void Start()
+    {
+        GameTime = CountDownTime;
+    }
+
+    void Update()
+    {
+        int M = (int)(GameTime / 60);
+        float S = GameTime % 60;
+        timer += Time.deltaTime;
+        if (timer >= 1f)
+        {
+            timer = 0;
+            GameTime--;
+            GameCountTimeText.text = "逃离倒计时：" + M + "：" + string.Format("{0:00}", S);
+        }
+    }
+}
